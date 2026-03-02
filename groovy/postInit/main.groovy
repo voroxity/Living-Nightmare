96393eb03b204ea19b414bdd367a50e35404a335
voroxity <mechlabgh@gmail.com>
2026-03-02 18:14:54 -0500

    general config and mod updates

--- groovy/postInit/main.groovy
+++ groovy/postInit/main.groovy
@@ -1,8 +1,16 @@
 
 log.info('postint loaded')
-//oredict
-//remove from the ore dict
+
+//remove recipe
+crafting.removeByOutput(item('malisisdoors:forcefielditem'))//force field door
+
+//Add recipe
+crafting.addShapeless(item('hbm:sulfur'), [item('thermalfoundation:material', 771)])//Thermal sulfur to NTM sulfur
+furnace.add(item('hbm:sulfur'), item('minecraft:gunpowder'))//NTM sulfur to gunpowder
+
+//DISSABLED
 /*
+//ordict
 ore_dict.remove('gunpowder', item('mwc:sulfur_dust'))
 ore_dict.remove('dustSulfur', item('mwc:sulfur_dust'))
 ore_dict.remove('dustSulfur', item('thermalfoundation:material', 771))
@@ -21,53 +29,27 @@ ore_dict.remove('ingotSilver', item('projectred-core:resource_item', 102))
 ore_dict.remove('oreTin', item('thermalfoundation:ore', 1))
 ore_dict.remove('oreClathrateOilSand', item('thermalfoundation:ore_fluid:', 0))
 ore_dict.remove('oreClathrateOilShale', item('thermalfoundation:ore_fluid', 1))
-*/
-//add to the ore dict
-/*
 ore_dict.add('oreLimestone', item('chisel:limestone:*'))
 ore_dict.add('oreLimestone', item('chisel:limestone1:*'))
 ore_dict.add('oreLimestone', item('chisel:limestone2:*'))
-*/
-//crafting
-//remove by output
-
-crafting.removeByOutput(item('malisisdoors:forcefielditem'))
-/*crafting.removeByOutput(item('thermalfoundation:material:128'))
+//crafting table
+crafting.removeByOutput(item('thermalfoundation:material:128'))
 crafting.removeByOutput(item('thermalfoundation:material:129'))
 crafting.removeByOutput(item('thermalfoundation:material:131'))
 crafting.removeByOutput(item('thermalfoundation:material:132'))
 crafting.removeByOutput(item('thermalfoundation:material:160'))
 crafting.removeByOutput(item('thermalfoundation:material:802'))
 crafting.removeByOutput(item('thermalfoundation:material', 771))
-*/
-//remove by input
-/*
 crafting.removeByInput(item('thermalfoundation:material:132'))
 crafting.removeByInput(item('thermalfoundation:material:802'))
-*/
-//crafting.removeByInput(item('thermalfoundation:material', 771))
-//add
-
-crafting.addShapeless(item('hbm:sulfur'), [item('thermalfoundation:material', 771)])
-
-//smelting
-//remove by input
-
-//furnace.removeByInput(item('mwc:sulfur_dust:*'))
-//furnace.removeByInput(item('mwc:copper_ore:*'))
-//furnace.removeByInput(item('mwc:lead_ore:*'))
-
-//remove by out put
-
-//furnace.removeByOutput(item('thermalfoundation:material', 771))
-
-//add
-
-furnace.add(item('hbm:sulfur'), item('minecraft:gunpowder'))
+crafting.removeByInput(item('thermalfoundation:material', 771))
+//furnace
 furnace.add(item('hbm:powder_quartz'), item('appliedenergistics2:material', 5))
-
-//jei
-/*
+furnace.removeByInput(item('mwc:sulfur_dust:*'))
+furnace.removeByInput(item('mwc:copper_ore:*'))
+furnace.removeByInput(item('mwc:lead_ore:*'))
+furnace.removeByOutput(item('thermalfoundation:material', 771))
+//JEI
 mods.jei.ingredient.hide(item('mwc:sulfur_dust'))
 mods.jei.ingredient.hide(item('mwc:sulfur_ore'))
 mods.jei.ingredient.hide(item('mwc:copper_ore'))
