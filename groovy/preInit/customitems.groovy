bd763981569545d0c2bc4e794da596d40abb6562
voroxity <mechlabgh@gmail.com>
2026-03-30 17:28:39 -0400

    replace custom items with mod

--- groovy/preInit/customitems.groovy
+++
@@ -1,5 +0,0 @@
-
-log.info('preInit customitems')
-
-content.createItem('D6').setEnchantedEffect().setRarity(EnumRarity.EPIC).setMaxStackSize(1).setFull3D().register()
-content.createItem('Luna').setEnchantedEffect().setRarity(EnumRarity.EPIC).setMaxStackSize(1).setFull3D().register()
